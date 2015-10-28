1a3b6fdfadce92dfd2f973f4f50333dd77d89764
Abigail Watson <awatson1978@gmail.com>
2015-10-27 19:55:44 -0700

    more updates to the review button

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 
 Package.describe({
   name: 'clinical:collaborations-ui',
-  version: '2.1.11',
+  version: '2.1.13',
   summary: 'UI components for the clinical:collaborations package.',
   git: 'https://github.com/clinical-meteor/clinical-collaborations-ui',
   documentation: 'README.md'
