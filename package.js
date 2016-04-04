8ff47e9693996295ed4f0b8694b99c32472f5d00
Abigail Watson <awatson1978@gmail.com>
2016-04-03 22:48:33 -0700

    bump and publish

--- package.js
+++ package.js
@@ -10,20 +10,21 @@ Package.describe({
 
 Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
+
   api.use([
     'meteor-platform@1.2.2',
     'http@1.1.0',
     'grove:less@0.1.1',
     'jquery@1.11.3_2',
     'underscore@1.0.3',
-    'clinical:collaborations@2.2.1',
+    'clinical:collaborations@2.3.8',
     'templating@1.1.1',
     'aldeed:template-extension@3.4.3',
     'cfs:http-methods@0.0.29',
     'aldeed:http@0.2.2',
-    'clinical:router@2.0.13',
-    'pfafman:font-awesome-4@4.2.0_4',
-    'clinical:glass-ui@1.3.9'
+    'clinical:router@2.0.17',
+    'clinical:fonts@1.1.0',
+    'clinical:glass-ui@1.3.12'
   ]);
 
 
